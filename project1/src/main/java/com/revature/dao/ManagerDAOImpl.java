13076545f9b406c92fd415f4cc92d23c737218aa
jgalena <jgalena1000@gmail.com>
2022-05-10 19:21:26 -0500

    Started ManagerDAOImpl

--- project1/src/main/java/com/revature/dao/ManagerDAOImpl.java
+++ project1/src/main/java/com/revature/dao/ManagerDAOImpl.java
@@ -2,26 +2,41 @@ package com.revature.dao;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import com.revature.models.Manager;
+import com.revature.util.HibernateUtil;
 
 public class ManagerDAOImpl implements ManagerDAO {
+	private static Logger log = Logger.getLogger(ManagerDAOImpl.class);
 
 	@Override
 	public int insert(Manager manager) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		log.info("Adding manager into the database");
+		
+		Session ses = HibernateUtil.getSession();
+		Transaction tx = ses.beginTransaction();
+		int pk = (int) ses.save(manager);
+		tx.commit();
+		
+		return pk;
 	}
 
 	@Override
 	public Manager selectById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Session ses = HibernateUtil.getSession();
+		Manager manager = (Manager) ses.createNativeQuery("SELECT * FROM system_managers WHERE system_manager_id = " + id + "", Manager.class).getSingleResult();
+		return manager;
 	}
 
 	@Override
 	public Manager selectByUsername(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		Session ses = HibernateUtil.getSession();
+		Manager manager = (Manager) ses.createNativeQuery("SELECT * FROM system_managers WHERE system_manager_username = " + username + "", Manager.class).getSingleResult();
+		return manager;
 	}
 
 	@Override
