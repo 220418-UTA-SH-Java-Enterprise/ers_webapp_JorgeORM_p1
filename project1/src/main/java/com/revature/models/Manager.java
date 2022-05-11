0c54577e3661e8960331e01ad40d10386dc3d7b7
jgalena <jgalena1000@gmail.com>
2022-05-11 16:11:47 -0500

    finished manager services

--- project1/src/main/java/com/revature/models/Manager.java
+++ project1/src/main/java/com/revature/models/Manager.java
@@ -8,11 +8,8 @@ import lombok.ToString;
 
 @Entity
 @Table(name="system_managers")
-@Getter
-@Setter
 @EqualsAndHashCode
 @ToString
-
 public class Manager {
 	
 	@Id
@@ -55,6 +52,54 @@ public class Manager {
 		this.password = password;
 		this.email = email;
 		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
--- project1/src/main/java/com/revature/services/ManagerServiceImpl.java
+++ project1/src/main/java/com/revature/services/ManagerServiceImpl.java
@@ -1,51 +1,57 @@
 package com.revature.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.revature.dao.ManagerDAO;
+import com.revature.dao.ManagerDAOImpl;
 import com.revature.models.Manager;
 
 public class ManagerServiceImpl implements ManagerService{
+	private ManagerDAO mdao;
+	
+	public ManagerServiceImpl(ManagerDAOImpl dao) {
+		super();
+		this.mdao = dao;
+	}
 
 	@Override
 	public Manager login(String username, String password) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Manager> managers = mdao.selectAll()
+			.stream()
+			.filter(m -> (m.getUsername().equals(username) && m.getPassword().equals(password)))
+			.findFirst();
+		return (managers.isPresent() ? managers.get() : null);
 	}
 
 	@Override
 	public int register(Manager manager) {
-		// TODO Auto-generated method stub
-		return 0;
+		return mdao.insert(manager);
 	}
 
 	@Override
 	public Manager findUserById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return mdao.selectById(id);
 	}
 
 	@Override
 	public Manager findUserByUsername(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		return mdao.selectByUsername(username);
 	}
 
 	@Override
 	public List<Manager> findAllManagers() {
-		// TODO Auto-generated method stub
-		return null;
+		return mdao.selectAll();
 	}
 
 	@Override
 	public boolean editManager(Manager manager) {
-		// TODO Auto-generated method stub
-		return false;
+		return mdao.update(manager);
 	}
 
 	@Override
 	public boolean deleteManager(Manager manager) {
-		// TODO Auto-generated method stub
-		return false;
+		return mdao.delete(manager);
 	}
 
 }
